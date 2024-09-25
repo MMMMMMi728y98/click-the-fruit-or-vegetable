004aa159d1cb6a3080a3857cf458bc1f27f439e7
Andrew Shin <A90909andrew@outlook.com>
2024-09-24 21:25:43 -0700

    Update docker-image.asm

--- .github/workflows/docker-image.asm
+++ .github/workflows/docker-image.asm
@@ -2,3 +2,10 @@ from imported content
   @URL IMPORT BOSE
   BOSE:self_asmcodefilechange
   lock2
+BITE MEGAdependencies:
+      '@fastify/compress':
+        specifier: ^6.5.0
+        version: 6.5.0
+      '@fastify/cookie':
+        specifier: ^9.3.1
+        version: 9.4.0
