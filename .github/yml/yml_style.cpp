3fddbb69d4b6bc162fa9624d02a500f4e4db53e0
Andrew Shin <A90909andrew@outlook.com>
2024-09-24 22:00:19 -0700

    Update yml_style.cpp

---
+++ .github/yml/yml_style.cpp
@@ -0,0 +1,43 @@
+  class dependemant bot install from cat
+  class:fucnormal:width:100; height:102
+  .catppuccin-frappe {
+    --background-primary: #303446 !important;
+    --background-lighter: #292c3c !important;
+    --navbar-color: #292c3c !important;
+    --navbar-height: 60px !important;
+    --navbar-text-color: #c6d0f5 !important;
+    --navbar-link-color: #8caaee !important;
+    --navbar-link-hover-color: #ca9ee6 !important;
+    --navbar-font: "Roboto" !important;
+    --input-text-color: #c6d0f5 !important;
+    --input-placeholder-color: #a5adce !important;
+    --input-background-color: #232634 !important;
+    --input-border-color: color-mix(in srgb, #232634, #ca9ee6 50%) !important;
+    --input-border-size: 1.3px !important;
+    --navbar-logo-filter: none !important;
+    --dropdown-option-hover-color: #232634 !important;
+    --tab-color: #414559 !important;
+    --active-color: #ca9ee6 !important;from export as .suspenseContainer {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+  background-color: #191724;
+}
+
+@keyframes pulse-brighter {
+  0%,
+  100% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.5;
+  root from style
+  }
+}
+
+.animate-pulse-brighter {
+  animation: pulse-brighter 2s infinite;
+  height: 11rem;
+}
