0279e9d092ba055d05de812b7c8263676369a55f
Andrew Shin <A90909andrew@outlook.com>
2024-09-25 14:38:32 -0700

    Create note.js

---
+++ .a MR.AGARD  PLS READ THIS/note.js
@@ -0,0 +1,5 @@
+set time from export from site?userlogintime
+  @function url from bose import ['https://www.github.com/MMMMMMi728y98/assets/data.cvs/cvxjs92.dexl']
+  @function url import code
+      BOSE.IMPORT = docs
+      BOSE.EXPORT = root
